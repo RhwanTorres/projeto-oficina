acf9ad6f33e296adfad4986be460050504b39837
Rhwan Torres <rhwantorres4@gmail.com>
2026-05-03 00:48:29 -0300

    tela de dashboard

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,33 @@ function login() {
 function irRecuperacao() {
   window.location.href = "recuperacao.html";
 }
+
+function irClientes() {
+  window.location.href = "clientes.html";
+}
+
+function irOrdens() {
+  window.location.href = "ordem.html";
+}
+
+function sair() {
+  window.location.href = "index.html";
+}
+
+function irDashboard() {
+  window.location.href = "dashboard.html";
+}
+
+function toggleMenu() {
+  const menu = document.getElementById("dropdown");
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+function irPerfil() {
+  window.location.href = "perfil.html";
+}
